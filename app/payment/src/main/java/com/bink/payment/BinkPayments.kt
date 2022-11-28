db21d31934689dd2133c828cefc2b2441fbe7c37
Joshua Best <Jbest@bink.com>
2022-11-28 19:16:52 +0000

    Added check for double init

--- app/payment/src/main/java/com/bink/payment/BinkPayments.kt
+++ app/payment/src/main/java/com/bink/payment/BinkPayments.kt
@@ -12,15 +12,18 @@ object BinkPayments {
     fun init(userToken: String, spreedlyEnvironmentKey: String, isDebug: Boolean) {
         if (userToken.isBlank()) throw NullPointerException("User token must not be null or blank")
         if (spreedlyEnvironmentKey.isBlank()) throw NullPointerException("Spreedly Environment Key must not be null or blank")
+        if (this::userToken.isInitialized && this::spreedlyEnvironmentKey.isInitialized) {
+            this.binkLogger.log(BinkLogger.LogType.ERROR, "Bink Payments SDK has already been initialized with User Token:${this.userToken} and Spreedly Environment Key:${this.spreedlyEnvironmentKey}")
+        } else {
+            this.userToken = userToken
+            this.spreedlyEnvironmentKey = spreedlyEnvironmentKey
+            this.isDebug = isDebug
+            this.binkLogger = BinkLogger(if (isDebug) BinkLogger.LogType.DEBUG else BinkLogger.LogType.VERBOSE)
 
-        this.userToken = userToken
-        this.spreedlyEnvironmentKey = spreedlyEnvironmentKey
-        this.isDebug = isDebug
-        this.binkLogger = BinkLogger(if (isDebug) BinkLogger.LogType.DEBUG else BinkLogger.LogType.VERBOSE)
-
-        this.binkLogger.log(BinkLogger.LogType.VERBOSE, "Bink Payments SDK Initialised")
-        this.binkLogger.log(BinkLogger.LogType.DEBUG, "User token set to $userToken")
-        this.binkLogger.log(BinkLogger.LogType.DEBUG, "Spreedly Environment Token set to $spreedlyEnvironmentKey")
+            this.binkLogger.log(BinkLogger.LogType.VERBOSE, "Bink Payments SDK Initialised")
+            this.binkLogger.log(BinkLogger.LogType.DEBUG, "User token set to $userToken")
+            this.binkLogger.log(BinkLogger.LogType.DEBUG, "Spreedly Environment Token set to $spreedlyEnvironmentKey")
+        }
     }
 
 }
